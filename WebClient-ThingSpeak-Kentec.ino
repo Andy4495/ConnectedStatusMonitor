01db66f4389bf96bf60f4fe655afdb334650ac58
Andreas Taylor <Andy4495@outlook.com>
2018-06-10 23:39:36 -0500

    Add support for Kentec display.

--- WebClient-ThingSpeak-Kentec.ino
+++ WebClient-ThingSpeak-Kentec.ino
@@ -14,6 +14,8 @@
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include "Screen_K35_SPI.h"
+Screen_K35_SPI myScreen;
 #include "ThingSpeakKeys.h"
 
 // Enter a MAC address for your controller below.
@@ -24,8 +26,10 @@ byte mac[] = LAUNCHPAD_MAC;
 const char* server = "api.thingspeak.com";
 
 char receiveBuffer[1024] = {};
+char printBuffer[32] = {};
+char prevPrintBuffer[32] = {};
 
-const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(16) + 510;
+const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(10) + JSON_OBJECT_SIZE(16) + 600;
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -46,11 +50,18 @@ void setup() {
   delay(1000);
   Serial.println("connecting...");
 
+  myScreen.begin();
+  myScreen.setPenSolid(true);
+  myScreen.setFontSolid(false);
+  myScreen.setFontSize(1);
+  myScreen.gText(0, 0, "ThingSpeak JSON Queries", whiteColour, blackColour, 2, 2);
+
 }
 
 void loop()
 {
   DynamicJsonBuffer jsonBuffer(bufferSize);
+
   int i = 0;
   char c;
 
@@ -64,8 +75,9 @@ void loop()
   }
 
   // Make a HTTP request:
-  //  client.println("GET /channels/412285/fields/1.csv?results=1");
-  client.println("GET /channels/" WEATHERSTATION_CHANNEL "/fields/3.json?api_key=" WEATHERSTATION_KEY "&results=1");
+  //  client.println("GET /channels/412285/fields/4.csv?results=1");
+  // client.println("GET /channels/" WEATHERSTATION_CHANNEL "/fields/4.json?api_key=" WEATHERSTATION_KEY "&results=1");
+  client.println("GET /channels/" WEATHERSTATION_CHANNEL "/feeds.json?api_key=" WEATHERSTATION_KEY "&results=1");
   client.println();
 
   // Need to check for connection and wait for characters
@@ -101,40 +113,61 @@ void loop()
 
   JsonObject& root = jsonBuffer.parseObject(receiveBuffer);
   /*
-  JsonObject& channel = root["channel"];
-  long channel_id = channel["id"]; // 379945
-  const char* channel_name = channel["name"]; // "Weather-Station"
-  const char* channel_description = channel["description"]; // "Outdoor weather station using MSP430 LaunchPad and SENSORS BoosterPack. "
-  const char* channel_latitude = channel["latitude"]; // "0.0"
-  const char* channel_longitude = channel["longitude"]; // "0.0"
-  const char* channel_field1 = channel["field1"]; // "Temp-BME280"
-  const char* channel_field2 = channel["field2"]; // "Temp-TMP007-Ext"
-  const char* channel_field3 = channel["field3"]; // "Temp-TMP007-Int"
-  const char* channel_field4 = channel["field4"]; // "Temp-MSP430"
-  const char* channel_field5 = channel["field5"]; // "RH-BME280"
-  const char* channel_field6 = channel["field6"]; // "Pressure-BME280"
-  const char* channel_field7 = channel["field7"]; // "Lux-OPT3001"
-  const char* channel_field8 = channel["field8"]; // "Batt-Vcc"
-  const char* channel_created_at = channel["created_at"]; // "2017-12-07T00:28:34Z"
-  const char* channel_updated_at = channel["updated_at"]; // "2018-06-10T22:26:22Z"
-  long channel_last_entry_id = channel["last_entry_id"]; // 90649 
+    JsonObject& channel = root["channel"];
+    long channel_id = channel["id"]; // 379945
+    const char* channel_name = channel["name"]; // "Weather-Station"
+    const char* channel_description = channel["description"]; // "Outdoor weather station using MSP430 LaunchPad and SENSORS BoosterPack. "
+    const char* channel_latitude = channel["latitude"]; // "0.0"
+    const char* channel_longitude = channel["longitude"]; // "0.0"
+    const char* channel_field1 = channel["field1"]; // "Temp-BME280"
+    const char* channel_field2 = channel["field2"]; // "Temp-TMP007-Ext"
+    const char* channel_field3 = channel["field3"]; // "Temp-TMP007-Int"
+    const char* channel_field4 = channel["field4"]; // "Temp-MSP430"
+    const char* channel_field5 = channel["field5"]; // "RH-BME280"
+    const char* channel_field6 = channel["field6"]; // "Pressure-BME280"
+    const char* channel_field7 = channel["field7"]; // "Lux-OPT3001"
+    const char* channel_field8 = channel["field8"]; // "Batt-Vcc"
+    const char* channel_created_at = channel["created_at"]; // "2017-12-07T00:28:34Z"
+    const char* channel_updated_at = channel["updated_at"]; // "2018-06-10T22:26:22Z"
+    long channel_last_entry_id = channel["last_entry_id"]; // 90649
   */
 
-  JsonObject& feeds0 = root["feeds"][0];
-  const char* feeds0_created_at = feeds0["created_at"]; // "2018-06-10T22:26:23Z"
-  long feeds0_entry_id = feeds0["entry_id"]; // 90649
-  const char* feeds0_field3 = feeds0["field3"]; // "669"
+  if (root.success()) {
 
-  long Tf = strtol(feeds0_field3, NULL, 10);
-  Serial.println("Parsed JSON: ");
-  Serial.print("Created at: ");
-  Serial.println(feeds0_created_at);
-  Serial.print("Entry ID: ");
-  Serial.println(feeds0_entry_id);
-  Serial.print("Temperature: ");
-  Serial.print(Tf / 10);
-  Serial.print(".");
-  Serial.println(Tf % 10);
+    JsonObject& feeds0 = root["feeds"][0];
+    const char* feeds0_created_at = feeds0["created_at"]; // "2018-06-10T22:26:23Z"
+    long feeds0_entry_id = feeds0["entry_id"]; // 90649
+    const char* feeds0_field3 = feeds0["field4"]; // "669"
+
+    long Tf = strtol(feeds0_field3, NULL, 10);
+    Serial.println("Parsed JSON: ");
+    Serial.print("Created at: ");
+    Serial.println(feeds0_created_at);
+    Serial.print("Entry ID: ");
+    Serial.println(feeds0_entry_id);
+    Serial.print("Temperature: ");
+    Serial.print(Tf / 10);
+    Serial.print(".");
+    Serial.println(Tf % 10);
+
+    snprintf(printBuffer, 32, "%i.%i", Tf / 10, Tf % 10);
+
+    myScreen.gText(0, 20, "Outdoor Temp: ");
+    myScreen.gText(80, 20, prevPrintBuffer, blackColour);
+    myScreen.gText(80, 20, printBuffer);
+    strncpy(prevPrintBuffer, printBuffer, 32);
+  }
+  else
+  {
+    Serial.println("JSON parse failed.");
+    
+    myScreen.gText(0, 20, "Outdoor Temp: ");
+    myScreen.gText(80, 20, prevPrintBuffer, blackColour);
+    myScreen.gText(80, 20, "N/A");
+    strncpy(prevPrintBuffer, "N/A", 32);
+
+  }
+
 
   Serial.println("Disconnecting. Waiting 30 seconds before next query. ");
 
